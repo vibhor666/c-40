59d520971e1fa8107dae5bf5c2535173d24cb7e4
vibhor666 <vibhordhanvijay03@gmail.com>
2021-05-10 15:52:58 +0530

    reset button

--- js/Game.js
+++ js/Game.js
@@ -55,7 +55,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 230 ;
       var y;
 
       for(var plr in allPlayers){
@@ -63,13 +63,16 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 225;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10)
+          fill("red")
+          ellipse(x,y,60,60)
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
